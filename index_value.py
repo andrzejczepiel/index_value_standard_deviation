885c1be30bbdea7e56f77145a1b617ddb107527e
Andrzej Czepiel <61347262+andrzejczepiel@users.noreply.github.com>
2020-06-08 19:54:01 +0200

    Add files via upload

---
+++ index_value.py
@@ -0,0 +1,20 @@
+import math
+
+class VolMove:
+    def __init__(self, vix: int=0, index: int=0, days: int=0):
+        self.vix = vix
+        self.index = index
+        self.days = days
+
+
+    def predicted_move(self):
+        predicted_move = (self.index * (self.vix / 100) * math.sqrt(self.days / 365))
+        return predicted_move
+
+
+
+
+today = VolMove(28, 3050, 46)
+
+
+print(today.predicted_move())
